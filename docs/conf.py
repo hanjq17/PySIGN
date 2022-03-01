a1ab25723bdcce686690630144c10dc5af5256aa
hanjq17 <hanjq17@mails.tsinghua.edu.cn>
2022-03-01 23:20:50 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'AirGeom'
 copyright = '2022, AirGeom Team, Tsinghua University'
-author = 'Jiaqi Han, Rui Jiao, Wenbing Huang, Yang Liu'
+author = 'AirGeom Team'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.0'
